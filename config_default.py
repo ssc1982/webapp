bdaebe3c345d6b0bee2926d7e6e5b0e591c0a90a
shawnsong <33675613+ssc1982@users.noreply.github.com>
2017-11-22 21:28:15 -0600

    Day 6
    Daay 6

---
+++ config_default.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+
+configs = {
+    'db':{
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'password',
+        'database': 'mysql'
+    },
+    'session':{
+        'secret': 'mysql'
+    }
+}
