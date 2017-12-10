dfe0a93c2e6ab0a40c9a91d9222f9aa304e28d60
shawnsong <33675613+ssc1982@users.noreply.github.com>
2017-12-09 20:00:33 -0600

    day 9

--- coroweb.py
+++ coroweb.py
@@ -10,8 +10,9 @@ from apis import APIError
 
 def get(path):
     def decorator(func):
-        @functools.wraps (func)
+        @functools.wraps(func)
         def wrapper(*args, **kw):
+            return func(*args, **kw)
         wrapper.__method__ = 'GET'
         wrapper.__route__ = path
         return wrapper
